6ec1685d1d6a1b82020d272e973892b253e9bfe9
Sven Meier <svenmeier@apache.org>
2019-07-02 09:05:23 +0200

    WICKET-6681 extend must preserve __proto__

--- wicket-core/src/main/java/org/apache/wicket/ajax/res/js/wicket-ajax-vanilla.js
+++ wicket-core/src/main/java/org/apache/wicket/ajax/res/js/wicket-ajax-vanilla.js
@@ -190,37 +190,35 @@
 	var extend = function () {
 
 		// Variables
-		var extended = {};
 		var deep = false;
 		var i = 0;
 		var length = arguments.length;
 
 		// Check if a deep merge
 		if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
-			deep = arguments[0];
+			deep = arguments[i];
 			i++;
 		}
 
-		// Merge the object into the extended object
-		var merge = function (obj) {
+		var extended = arguments[i];
+		i++;
+		
+		// Loop through each object and conduct a merge
+		for ( ; i < length; i++ ) {
+			var obj = arguments[i];
+			
 			for ( var prop in obj ) {
 				if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
 					// If deep merge and property is an object, merge properties
-					if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+					if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' && extended[prop]) {
 						extended[prop] = extend( true, extended[prop], obj[prop] );
 					} else {
 						extended[prop] = obj[prop];
 					}
 				}
 			}
-		};
-
-		// Loop through each object and conduct a merge
-		for ( ; i < length; i++ ) {
-			var obj = arguments[i];
-			merge(obj);
 		}
-
+		
 		return extended;
 	};
 	
@@ -1172,7 +1170,7 @@
 		}
 	};
 
-	Wicket = extend(true, Wicket, {
+	extend(true, Wicket, {
 
 		channelManager: new Wicket.ChannelManager(),
 
